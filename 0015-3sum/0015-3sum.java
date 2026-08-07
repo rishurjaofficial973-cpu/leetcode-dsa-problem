999cf18c9b8cd42dad4c8a9381a4b5a348ad11fa
RISHU RAJ <rishurja.official973@gmail.com>
2026-08-07 06:52:04 +0530

    Time: 34 ms (60.94%), Space: 59.1 MB (60.33%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,13 +1,33 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+    //     Set<List<Integer>> set = new HashSet<>();
+    //     int n = nums.length;
+    //   for (int i = 0; i < n;i++){
+    //     for(int j = i+1; j< n;j++){
+    //         for(int k = j+1; k<n ;k++){
+
+    //             if(nums[i]+nums[j]+nums[k] == 0) {
+    //                 List<Integer> tikkisum = Arrays.asList(nums[i],nums[j],nums[k]);
+    //                 Collections.sort(tikkisum);
+    //                 set.add(tikkisum);
+    //             }
+    //         }
+    //     }
+        
+
+    //   }  
+    //           return new ArrayList<>(set);
+    // optimize baad me kar lenge bhai !!!
+    // AB HOGA OPTIMIZE !!----- 
+     List<List<Integer>> ans = new ArrayList<>();
+
         Arrays.sort(nums);
 
         int n = nums.length;
 
         for (int i = 0; i < n - 2; i++) {
+
+          
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
@@ -19,26 +39,32 @@ class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
                     left++;
                     right--;
-                } else if (sum < 0) {
+
+                 
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+                else if (sum < 0) {
                     left++;
-                } else {
+                }
+                else {
                     right--;
                 }
             }
         }
 
-        return result;
+        return ans;
+
+
     }
 }
+
